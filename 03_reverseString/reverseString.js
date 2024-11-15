201cc40ae8a60c38f211ca4681454c056913f333
Vess <26VessN@badinhs.org>
2024-11-15 13:51:49 -0500

    Done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
+const reverseString = function(String) {
+    return String.split('').reverse().join('')
 
 };
-
+console.log (reverseString('hello there'));
 // Do not edit below this line
 console.log(reverseString);
