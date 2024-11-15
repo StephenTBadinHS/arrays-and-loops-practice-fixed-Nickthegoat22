090c00232c2a260361d6890ea68f99adb3401a77
Vess <26VessN@badinhs.org>
2024-11-15 13:52:50 -0500

    done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...valuesToRemove) {
+    return array.filter(item => !valuesToRemove.includes(item));
 };
+console.log(removeFromArray([1 , 2 , 3 , 4], 3))    
+
+
 
 // Do not edit below this line
 console.log(removeFromArray);
